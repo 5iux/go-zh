1401ed4880c6e3df7b81ffcae415bb114c411fe9
D.Young <13775651+5iux@users.noreply.github.com>
2019-05-21 10:16:43 +0800

    Add files via upload

---
+++ go-zh.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @namespace     https://github/5iux
+// @name          Google中文
+// @description   强制google搜索更改成中文,脚本基于英文版https://openuserjs.org/scripts/fardjad/Google_in_English_PLEASE! 更改
+// @author        dyoung
+// @version       0.0.1
+// @include       *://*.google.*
+// @grant         none
+// ==/UserScript==
+
+
+(function () {
+  'use strict';
+  const urlParams = new URLSearchParams(window.location.search);
+  const hl = urlParams.get('hl');
+  if (hl == null) {
+    urlParams.append('hl', 'zh-CN');
+    const {
+      protocol,
+      host,
+      pathname,
+      hash
+    } = window.location;
+    window.location.replace(protocol + '//' + host + pathname + '?' + urlParams.toString() + hash);
+  }
+})();
